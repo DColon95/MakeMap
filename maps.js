9aabf6e2d6221f1d0134cfe01bde2e069f08fee0
Dario Colon <dario.a.colon@gmail.com>
2022-12-02 19:02:14 -0500

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,30 @@
+const scale = 2; // Accepted values are 1 and 2 
+const zoom = 20; // 1: World, 5: Landmass/continent, 10: City, 15: Streets, 20: Buildings
+const size = '640x640'; // This parameter takes a string of the form {horizontal_value}x{vertical_value}
+
+/* This table shows the maximum allowable values for the size parameter at each scale value.
+| scale=1 | scale=2                            |
+| ------- | ---------------------------------- |
+| 640x640 | 640x640 (returns 1280x1280 pixels) |*/
+
+const key = 'AIzaSyB_PlhsyhxT7KhzJR4BFmtT6EN6sn764ig';
+
+document.getElementById('address-form').addEventListener('submit', makeMaps);
+
+function makeMaps(submit) {
+    submit.preventDefault();
+
+    let center = document.getElementById('address').value;
+    let locationMapURL = 'https://maps.googleapis.com/maps/api/staticmap?center=' + URLEncode(center) + '&zoom=' + zoom + '&size=' + size + '&scale=' + scale + '&markers=color:red|' + URLEncode(center) + '&key=' + key;
+    let aerialMapURL = locationMapURL + '&maptype=satellite';
+
+    let aerialMap = document.getElementById('aerial');
+    let locationMap = document.getElementById('location');
+
+    aerialMap.src = aerialMapURL;
+    locationMap.src = locationMapURL;
+}
+
+function URLEncode(text) {
+    return text.replaceAll(' ', '+');
+}
